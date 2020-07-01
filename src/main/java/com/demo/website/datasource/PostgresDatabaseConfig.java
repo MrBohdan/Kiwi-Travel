7e3934958afb4da4c5d16b936e2d2f6885b5039e
Bohdan Skrypnyk <mrbudda2012@gmail.com>
2020-07-01 17:21:19 +0300

    init com

--- src/main/java/com/demo/website/datasource/PostgresDatabaseConfig.java
+++ src/main/java/com/demo/website/datasource/PostgresDatabaseConfig.java
@@ -1,20 +1,33 @@
 package com.demo.website.datasource;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PostgresDataSource {
+public class PostgresDatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
     @Bean
+    @ConfigurationProperties("spring.datasource")
+    public HikariConfig hikariConfig(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+
+    /**@Bean
     @ConfigurationProperties("spring.datasource")
     public HikariDataSource hikariDataSource(){
         return DataSourceBuilder
                 .create()
                 .type(HikariDataSource.class)
                 .build();
-    }
+    }*/
 }
