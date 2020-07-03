ecc635f77c7eb67a380cdb49b951bb7cb144a698
Bohdan Skrypnyk <mrbudda2012@gmail.com>
2020-07-03 15:55:23 +0300

    init com

--- src/main/java/com/demo/website/controller/MainController.java
+++ src/main/java/com/demo/website/controller/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
-
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
 }
