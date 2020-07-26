e3654f7ac8b7b03588d14aa0e909e7090f0354f0
Bohdan Skrypnyk <mrbudda2012@gmail.com>
2020-07-26 23:41:39 +0300

    pegination

--- src/main/java/com/demo/website/controller/PostController.java
+++ src/main/java/com/demo/website/controller/PostController.java
@@ -2,13 +2,10 @@ package com.demo.website.controller;
 
 import com.demo.website.model.Post;
 import com.demo.website.repository.PostsRepository;
-import org.hibernate.jpa.QueryHints;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.lang.NonNull;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,8 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.persistence.EntityManager;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.apache.http.entity.ContentType.*;
 
@@ -55,43 +50,26 @@ public class PostController {
         return postsRepository.save(post);
     }
 
-   /* @GetMapping(value = "/get") // Map ONLY GET Requests
-    public Page<Post> findAll(Pageable pageable){
-        return postsRepository.findAll(pageable);
-    }*/
-    @GetMapping(value = "/get") // Map ONLY GET Requests
-   @Transactional
-    public @ResponseBody List<Post> findAll() {
-        try(Stream<Post> postStream = entityManager
-                .createQuery(
-                        "select p " +
-                                "from post p " +
-                                "order by p.zonedDateTime desc", Post.class)
-                .setHint( QueryHints.HINT_FETCH_SIZE, 20 )
-                .unwrap(Query.class)
-                .stream()
-        ) {
-            return postStream
-                    .limit( 20 )
-                    .collect(
-                            Collectors.toList()
-                    );
-        }
-    }
+     @GetMapping(value = "/get") // Map ONLY GET Requests
+     public @ResponseBody
+     Page<Post> findAll(Pageable pageable){
+         return postsRepository.findAll(pageable);
+     }
+
 
     @DeleteMapping(value = "/delete/{post_Id}") // Map ONLY DELETE Requests
-    public void removePostByPostId(@PathVariable("post_Id") UUID post_Id){
+    public void removePostByPostId(@PathVariable("post_Id") UUID post_Id) {
         postsRepository.deleteById(post_Id);
     }
 
-    public void isFileEmpty(MultipartFile file){
-        if(file.isEmpty()){
-            throw new IllegalStateException("Cannot upload empty file [" + file.getSize()+"]");
+    public void isFileEmpty(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() + "]");
         }
     }
 
-    public void isImage(MultipartFile file){
-        if(!Arrays.asList(IMAGE_JPEG.getMimeType(), IMAGE_PNG.getMimeType()).contains(file.getContentType())){
+    public void isImage(MultipartFile file) {
+        if (!Arrays.asList(IMAGE_JPEG.getMimeType(), IMAGE_PNG.getMimeType()).contains(file.getContentType())) {
             throw new IllegalStateException("File must be an image [" + file.getContentType() + "]");
         }
     }
--- src/main/java/com/demo/website/repository/PostsRepository.java
+++ src/main/java/com/demo/website/repository/PostsRepository.java
@@ -6,18 +6,10 @@ import com.demo.website.util.GenerateZonedDateTimeUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.QueryHint;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Stream;
-
-import static org.hibernate.annotations.QueryHints.READ_ONLY;
-import static org.hibernate.jpa.QueryHints.*;
 
 /**
  *  Through this repository, an object 'Post' will be saved to the database
@@ -26,11 +18,5 @@ import static org.hibernate.jpa.QueryHints.*;
 @Transactional
 public interface PostsRepository extends JpaRepository<Post, UUID>, GenerateUuidUtil, GenerateZonedDateTimeUtil {
 
-  //  @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "20"))
-    //@Query("select p from post p")
-    List<Post> findAll();
-
-
-    // https://medium.com/@avinash28196/spring-boot-pagination-implementation-in-react-js-part-1-cb363b111693
-    /*Page<Post> findAll(Pageable pageable);*/
+    Page<Post> findAll(Pageable pageable);
 }
