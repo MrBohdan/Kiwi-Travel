3d9be90cd7fa3ca0c798e5ba9d8bd7064fec53bf
Bohdan Skrypnyk <mrbudda2012@gmail.com>
2020-10-06 18:00:44 +0300

    testing

--- src/main/java/com/kiwi/website/config/ResourceFilesConfig.java
+++ src/main/java/com/kiwi/website/config/ResourceFilesConfig.java
@@ -1,6 +1,5 @@
 package com.kiwi.website.config;
 
-import com.kiwi.website.controller.reporting.GoogleAnalyticsReportingController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,6 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.logging.Logger;
 
 /**
  * Inject google analytics API V4 service account JSON
@@ -22,21 +20,17 @@ import java.util.logging.Logger;
 @Configuration
 public class ResourceFilesConfig {
 
-    private final static Logger LOGGER = Logger.getLogger(ResourceFilesConfig.class.getName());
-
     @Value("classpath:Kiwi-Travel-e0816208fef3.json")
     private Resource jsonFile;
 
     @Bean(name = "jsonFile")
     public File getKeyFile() {
         System.out.println(">>>>>>>>>>>>> ResourceFilesConfig >>>>>>>>>>>>>>>" + jsonFile);
-        LOGGER.info("Service account JSON received: " + jsonFile);
         try (InputStream is = jsonFile.getInputStream()) {
             Path temp = Files.createTempFile("resource-", ".ext");
             Files.copy(is, temp, StandardCopyOption.REPLACE_EXISTING);
             return temp.toFile();
         } catch (IOException e) {
-            LOGGER.info("Service account JSON received: FILE NOT FOUND" + jsonFile);
             System.out.println(">>>>>>>>>>>>> ResourceFilesConfig >>>>>>>>>>>>>>> FILE NOT FOUND");
             e.printStackTrace();
         }
